d3f89ff8bf2dbbbf813f5a795f5c77f342288ae1
Elaidzha1940 <eli080293@gmail.com>
2023-05-10 09:42:15 +0700

    added a new File / HikeGraph

---
+++ Views and Transitions/Views and Transitions/Hikes/HikeGraph.swift
@@ -0,0 +1,22 @@
+//  /*
+//
+//  Project: Views and Transitions
+//  File: HikeGraph.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 10.05.2023
+//
+//  */
+
+import SwiftUI
+
+struct HikeGraph: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HikeGraph_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeGraph()
+    }
+}
