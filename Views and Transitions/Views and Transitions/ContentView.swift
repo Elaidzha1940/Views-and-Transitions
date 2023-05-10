7c24e300370b379d6040d958721f04466ffd2919
Elaidzha1940 <eli080293@gmail.com>
2023-05-10 09:05:03 +0700

    git branch -M main

---
+++ Views and Transitions/Views and Transitions/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Views and Transitions
+//
+//  Created by Elaidzha Shchukin on 10.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Views and Transitions/Views and Transitions/Views_and_TransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Views_and_TransitionsApp.swift
+//  Views and Transitions
+//
+//  Created by Elaidzha Shchukin on 10.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Views_and_TransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
