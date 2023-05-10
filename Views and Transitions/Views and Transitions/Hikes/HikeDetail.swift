8c543836fad0b54c8d3d9ca33562f23937f419fe
Elaidzha1940 <eli080293@gmail.com>
2023-05-10 09:44:50 +0700

    added a new File / HikeDetail

---
+++ Views and Transitions/Views and Transitions/Hikes/HikeDetail.swift
@@ -0,0 +1,20 @@
+//
+//  HikeDetail.swift
+//  Views and Transitions
+//
+//  Created by Elaidzha Shchukin on 10.05.2023.
+//
+
+import SwiftUI
+
+struct HikeDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HikeDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeDetail()
+    }
+}
