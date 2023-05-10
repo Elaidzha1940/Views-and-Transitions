71c551370d73d10c1009112ef1eec0e50c8a43cf
Elaidzha1940 <eli080293@gmail.com>
2023-05-10 09:26:41 +0700

    Views and Transitions + added a new Folder and File / HikeView

---
+++ Views and Transitions/Views and Transitions/Hikes/HikeView.swift
@@ -0,0 +1,22 @@
+//  /*
+//
+//  Project: Views and Transitions
+//  File: HikeView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 10.05.2023
+//
+//  */
+
+import SwiftUI
+
+struct HikeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HikeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeView()
+    }
+}
